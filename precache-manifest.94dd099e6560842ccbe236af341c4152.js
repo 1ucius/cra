e95560f10070ef8ca0aa8bb24ea464cfd66f4534
1ucius <take.eat.ezzy@gmail.com>
2018-11-19 19:17:53 +0200

    Updates

---
+++ precache-manifest.94dd099e6560842ccbe236af341c4152.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "69de45ca6280bb4ddd86b683279d512a",
+    "url": "/cra/static/media/volume.69de45ca.svg"
+  },
+  {
+    "revision": "5004d33035b13b3fbb16dba79c8a786c",
+    "url": "/cra/static/media/play.5004d330.svg"
+  },
+  {
+    "revision": "07100aaba90a974f1387519c86c705b4",
+    "url": "/cra/static/media/pause.07100aab.svg"
+  },
+  {
+    "revision": "02d30e174c5738cefa5bdd0aefe2e874",
+    "url": "/cra/static/media/logo.02d30e17.svg"
+  },
+  {
+    "revision": "2e0a8bf78f0c600613b5",
+    "url": "/cra/static/js/runtime~main.2e0a8bf7.js"
+  },
+  {
+    "revision": "ddefd8933c8c55ae63c8",
+    "url": "/cra/static/js/main.ddefd893.chunk.js"
+  },
+  {
+    "revision": "64c58341ff77ab5ff3d6",
+    "url": "/cra/static/js/1.64c58341.chunk.js"
+  },
+  {
+    "revision": "ddefd8933c8c55ae63c8",
+    "url": "/cra/static/css/main.3e89ff6c.chunk.css"
+  },
+  {
+    "revision": "7135eb8e9522563255752599e9fc7fab",
+    "url": "/cra/index.html"
+  }
+];
