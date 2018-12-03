612585c464afb3b3c4f86d1c244e6f686bb1f4af
1ucius <take.eat.ezzy@gmail.com>
2018-12-03 13:24:24 +0200

    Updates

--- service-worker-custom.js
+++ service-worker-custom.js
@@ -38,7 +38,7 @@ workbox.routing.registerRoute(
 workbox.routing.registerRoute(
     new RegExp('^https://assets.planetradio.co.uk/audio-on-demand/radio-1-norge/'),
     workbox.strategies.cacheFirst({
-        cacheName: 'images-cache',
+        // cacheName: 'images-cache',
         plugins: [
             new workbox.cacheableResponse.Plugin({
                 statuses: [0, 200],
